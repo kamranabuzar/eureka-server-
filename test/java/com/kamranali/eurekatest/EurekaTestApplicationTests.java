2a15c4565706aad8b058545ba978a9dd1b00e6ad
kamranabuzar <56295632+kamranabuzar@users.noreply.github.com>
2020-01-08 04:21:34 +0300

    first commit

---
+++ test/java/com/kamranali/eurekatest/EurekaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamranali.eurekatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
